cb04cfaf35bab0759dfcc1a46cc177bd39fd8077
tflinch <tflinch16@gmail.com>
2024-07-27 19:59:01 -0400

    feat: Create Game class

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,13 @@
 from capitals import states
 
 print(states)
+
+class Game():
+    def __init__(self,):
+        self.rightTallies = 0
+        self.wrongTallies = 0
+        self.gameStates = states
+
+    
+
+        
