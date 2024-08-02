ec5285b327292c000401fcb54a1bde6e533219c2
Miagotobene <miagotobene@gmail.com>
2024-08-01 23:03:11 -0400

    feat: solution to state capital lab

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,36 @@
 from capitals import states
+# import random 
+import random
 
-print(states)
+restart = True
+while restart:
+    random.shuffle(states)
+    wrong = 0
+    correct = 0
+    print("""===============WELCOME=====================""")
+
+    question = input("Would you like to play the capital quiz game? [Yes/No]")
+    if question == "No":
+        restart = False
+        print("Oh...Okay")
+
+    if question == "Yes":
+        print("You'll be asked to guess the capital of the 50 states. Let's start!")
+        for state in (states[:50]):
+            question = input("What is the capital of " + state["name"]+ "? ")
+            if question != state["capital"]:
+                wrong = wrong + 1 
+                print("Wrong answer. Try again!")
+            if question == state["capital"]:
+                correct = correct + 1
+                print("Correct answer. You've earned one point!")
+        print("Game over ", "You have", correct, "answers and",  (wrong), "answers" )
+        question = input("Do you want to play again? [Yes/No]")
+        if question == "Yes":
+            restart = True
+        if question == "No":
+            print("Bye. See you next time!")
+            restart = False
+
+
+print(len(states))
